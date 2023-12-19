3f3edfa06e227077648aafa2725f75454e16f297
francisojeah <ojeahfrancis@gmail.com>
2023-12-19 13:58:28 +0100

    worked on nestjs setup

--- apps/silms-backend/src/app.module.ts
+++ apps/silms-backend/src/app.module.ts
@@ -1,16 +1,63 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { ScheduleModule } from '@nestjs/schedule';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+  utilities as nestWinstonModuleUtilities,
+  WinstonModule,
+} from 'nest-winston';
+import * as winston from 'winston';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { ErrorFilter } from './filters/error.filter';
+import { Logger, MiddlewareConsumer, Module } from '@nestjs/common';
+import { ErrorInterceptor } from './interceptors/error.interceptor';
+import { RequestLoggerMiddleware } from './utils/request-logger.middleware';
+
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
     ServeStaticModule.forRoot({
-    rootPath: join(__dirname, '../..', 'silms-frontend','dist'),
-    })
-   ],
+      rootPath: join(__dirname, '../..', 'silms-frontend', 'dist'),
+      exclude: ['/api*'],
+    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    WinstonModule.forRoot({
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.ms(),
+            nestWinstonModuleUtilities.format.nestLike('MyApp', {
+              colors: true,
+              prettyPrint: true,
+            }),
+          ),
+        }),
+      ],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    Logger,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ErrorInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: ErrorFilter,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {}
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestLoggerMiddleware).forRoutes('*');
+  }
+}
+
--- apps/silms-backend/src/main.ts
+++ apps/silms-backend/src/main.ts
@@ -1,10 +1,73 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { ValidationPipe } from '@nestjs/common';
+import { WinstonModule } from 'nest-winston';
+import { transports, format } from 'winston';
+import 'winston-daily-rotate-file';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new transports.DailyRotateFile({
+          filename: `logs/%DATE%-error.log`,
+          level: 'error',
+          format: format.combine(format.timestamp(), format.json()),
+          datePattern: 'YYYY-MM-DD',
+          zippedArchive: false,
+          maxFiles: '30d',
+        }),
+        new transports.DailyRotateFile({
+          filename: `logs/%DATE%-combined.log`,
+          format: format.combine(format.timestamp(), format.json()),
+          datePattern: 'YYYY-MM-DD',
+          zippedArchive: false,
+          maxFiles: '30d',
+        }),
+        new transports.Console({
+          format: format.combine(
+            format.cli(),
+            format.splat(),
+            format.timestamp(),
+            format.printf((info) => {
+              return `${info.timestamp} ${info.level}: ${info.message}`;
+            }),
+          ),
+        }),
+      ],
+    }),
+  });
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle(process.env.APP_NAME)
+    .setDescription(process.env.APP_DESCRIPTION)
+    .setVersion(process.env.API_VERSION)
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig, {
+    ignoreGlobalPrefix: false,
+  });
+
+  try {
+    SwaggerModule.setup('api', app, document);
+  } catch (error) {
+    this.logger.error(error);
+  }
+
+  // app.enableCors({ origin: [allowed] });
+  app.enableCors();
+
+  app.useGlobalPipes(new ValidationPipe());
+
   app.setGlobalPrefix('/backend');
 
-  await app.listen(3000);
+  await app.listen(
+    process.env.APP_SERVER_LISTEN_PORT,
+    process.env.APP_SERVER_LISTEN_IP,
+  );
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
