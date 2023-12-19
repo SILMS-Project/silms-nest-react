a6a8a4e3f26d066dd09fcf2a8d0fda4d6583596f
francisojeah <ojeahfrancis@gmail.com>
2023-12-19 14:16:39 +0100

    worked on the frontend setup

--- apps/silms-frontend/src/components/Demo.tsx
+++ apps/silms-frontend/src/components/Demo.tsx
@@ -1,35 +1,53 @@
-import { Button } from 'flowbite-react';
+import { useEffect, useState } from "react";
+import reactLogo from "../assets/react.svg";
+import viteLogo from "/vite.svg";
+import "flowbite";
 
 const Demo: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  const [message, setMessage] = useState("");
+  const fetchBackend = async () => {
+    try {
+      const message = (await fetch("/backend")).text();
+      setMessage(await message);
+    } catch (error: unknown) {
+      setMessage("");
+    }
+  };
+
+  useEffect(() => {
+    fetchBackend();
+  }, []);
+
   return (
-    <div className="flex flex-wrap gap-2">
-      <Button pill>Default</Button>
-      <Button color="blue" pill>
-        Blue
-      </Button>
-      <Button color="gray" pill>
-        Gray
-      </Button>
-      <Button color="dark" pill>
-        Dark
-      </Button>
-      <Button color="light" pill>
-        Light
-      </Button>
-      <Button color="success" pill>
-        Success
-      </Button>
-      <Button color="failure" pill>
-        Failure
-      </Button>
-      <Button color="warning" pill>
-        Warning
-      </Button>
-      <Button color="purple" pill>
-        Purple
-      </Button>
-    </div>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+        <p>
+          <h3>Message from the backend:</h3>
+          <h1>{message}</h1>
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   );
-}
+};
 
 export default Demo;
--- apps/silms-frontend/src/pages/Home/HomePage.tsx
+++ apps/silms-frontend/src/pages/Home/HomePage.tsx
@@ -3,7 +3,6 @@ import Demo from "@/components/Demo";
 const HomePage = () => {
   return (
     <>
-      <div>HomePage</div>
       <Demo />
     </>
   );
