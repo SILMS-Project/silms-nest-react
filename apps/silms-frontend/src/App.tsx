286940336323d7faacfb83d5ae33b0ca1782cc6b
francisojeah <ojeahfrancis@gmail.com>
2023-12-19 09:02:10 +0100

    setup routing using react router dom

--- apps/silms-frontend/src/App.tsx
+++ apps/silms-frontend/src/App.tsx
@@ -1,43 +1,39 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import Demo from "./components/Demo";
-import 'flowbite';
+import { Suspense, lazy, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "flowbite";
 import { initFlowbite } from "flowbite";
+import PageLoader from "./components/PageLoader";
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   useEffect(() => {
     initFlowbite();
   }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <p>
-          <Demo />
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<PageLoader />}>
+              {React.createElement(lazy(() => import("@/pages/Home/HomePage")))}
+            </Suspense>
+          }
+        />
+
+        <Route
+          path="*"
+          element={
+            <Suspense fallback={<PageLoader />}>
+              {React.createElement(
+                lazy(() => import("@/pages/Errors/NotFound"))
+              )}
+            </Suspense>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ apps/silms-frontend/src/pages/Home/HomePage.tsx
@@ -0,0 +1,12 @@
+import Demo from "@/components/Demo";
+
+const HomePage = () => {
+  return (
+    <>
+      <div>HomePage</div>
+      <Demo />
+    </>
+  );
+};
+
+export default HomePage;
