853d12f0df1bf13a0f8bcb2de1ca8ef4fee11d66
francisojeah <ojeahfrancis@gmail.com>
2023-12-22 12:57:16 +0100

    implemented auth entity

--- apps/silms-backend/src/modules/users/entities/user.entity.ts
+++ apps/silms-backend/src/modules/users/entities/user.entity.ts
@@ -1,26 +1,6 @@
-import { Role } from '@/utils/constants';
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity } from "typeorm";
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
-
-  @Column({ unique: true })
-  email: string;
-
-  @Column()
-  password: string;
-
-  @Column({ type: 'enum', enum: Role, default: Role.Student })
-  role: Role;
-
-  @Column({ default: true })
-  isVerified: boolean;
-
-  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
-  createdAt: Date;
-
-  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
-  updatedAt: Date;
+  
 }
