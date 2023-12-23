8b8dc5f877660463de119881d460b40473981b22
OGB <chidubem.info@gmail.com>
2023-12-23 22:54:46 +0100

    school,program and course entity relationship created

--- apps/silms-backend/src/modules/courses/entities/course.entity.ts
+++ apps/silms-backend/src/modules/courses/entities/course.entity.ts
@@ -1,1 +1,40 @@
-export class Course {}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { Program } from '@modules/programs/entities/program.entity';
+
+
+@Entity('courses')
+export class Course {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  courseCode: string;
+
+  @Column()
+  courseTitle: string;
+
+  @Column({ type: 'text', nullable: true })
+  description: string;
+
+  @Column({ type: 'int' })
+  unit: number;
+
+  @Column({ type: 'int' })
+  semester: number;
+
+  @Column({ type: 'text', nullable: true })
+  image: string;
+
+  @ManyToOne(() => Program, program => program.courses)
+  program: Program;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+}
--- apps/silms-backend/src/modules/programs/entities/program.entity.ts
+++ apps/silms-backend/src/modules/programs/entities/program.entity.ts
@@ -1,1 +1,34 @@
-export class Program {}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from 'typeorm';
+import { School } from '@modules/schools/entities/school.entity';
+import { Course } from '@modules/courses/entities/course.entity';
+
+@Entity('programs')
+export class Program {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  programName: string;
+
+  @Column({ type: 'text', nullable: true })
+  description: string;
+
+  @OneToMany(() => Course, course => course.program)
+  courses: Course[];
+
+  @Column({ type: 'text', nullable: true })
+  requirements: string;
+
+  @ManyToOne(() => School, school => school.programs)
+  school: School;
+  
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+}
