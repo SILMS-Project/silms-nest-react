ce3604edd4a16a1615c8bddcbac654863d08daa0
Lotanna Okeke <lotannaokeke@Lotannas-MacBook-Pro.local>
2023-12-23 08:40:48 +0100

    schools.controller route decorated

--- apps/silms-backend/src/modules/schools/schools.controller.ts
+++ apps/silms-backend/src/modules/schools/schools.controller.ts
@@ -1,33 +1,56 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { SchoolsService } from './schools.service';
 import { CreateSchoolDto } from './dto/create-school.dto';
 import { UpdateSchoolDto } from './dto/update-school.dto';
 
+@ApiTags('schools')
 @Controller('schools')
 export class SchoolsController {
   constructor(private readonly schoolsService: SchoolsService) {}
 
+  // @ApiOperation is added to provide information about the endpoint in Swagger documentation
+  // @ApiResponse is added to specify the response status and description
+
+  // POST /schools
   @Post()
+  @ApiOperation({ summary: 'Create a new school' })
+  @ApiResponse({ status: 201, description: 'School successfully created' })
   create(@Body() createSchoolDto: CreateSchoolDto) {
     return this.schoolsService.create(createSchoolDto);
   }
 
+  // GET /schools
   @Get()
+  @ApiOperation({ summary: 'Get all schools' })
+  @ApiResponse({ status: 200, description: 'List of all schools' })
   findAll() {
     return this.schoolsService.findAll();
   }
 
+  // GET /schools/:id
   @Get(':id')
+  @ApiOperation({ summary: 'Get a school by ID' })
+  @ApiResponse({ status: 200, description: 'School found by ID' })
+  @ApiResponse({ status: 404, description: 'School not found' })
   findOne(@Param('id') id: string) {
     return this.schoolsService.findOne(+id);
   }
 
+  // PATCH /schools/:id
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a school by ID' })
+  @ApiResponse({ status: 200, description: 'School successfully updated' })
+  @ApiResponse({ status: 404, description: 'School not found' })
   update(@Param('id') id: string, @Body() updateSchoolDto: UpdateSchoolDto) {
     return this.schoolsService.update(+id, updateSchoolDto);
   }
 
+  // DELETE /schools/:id
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a school by ID' })
+  @ApiResponse({ status: 200, description: 'School successfully deleted' })
+  @ApiResponse({ status: 404, description: 'School not found' })
   remove(@Param('id') id: string) {
     return this.schoolsService.remove(+id);
   }
