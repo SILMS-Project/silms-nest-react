429d848d59552995ad6ebfbbe5d959fe0eaddf25
Lotanna Okeke <lotannaokeke@Lotannas-MacBook-Pro.local>
2023-12-23 08:42:56 +0100

    students.controller route decorated

--- apps/silms-backend/src/modules/students/students.controller.ts
+++ apps/silms-backend/src/modules/students/students.controller.ts
@@ -1,33 +1,56 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { StudentsService } from './students.service';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
 
+@ApiTags('students')
 @Controller('students')
 export class StudentsController {
   constructor(private readonly studentsService: StudentsService) {}
 
+  // @ApiOperation is added to provide information about the endpoint in Swagger documentation
+  // @ApiResponse is added to specify the response status and description
+
+  // POST /students
   @Post()
+  @ApiOperation({ summary: 'Create a new student' })
+  @ApiResponse({ status: 201, description: 'Student successfully created' })
   create(@Body() createStudentDto: CreateStudentDto) {
     return this.studentsService.create(createStudentDto);
   }
 
+  // GET /students
   @Get()
+  @ApiOperation({ summary: 'Get all students' })
+  @ApiResponse({ status: 200, description: 'List of all students' })
   findAll() {
     return this.studentsService.findAll();
   }
 
+  // GET /students/:id
   @Get(':id')
+  @ApiOperation({ summary: 'Get a student by ID' })
+  @ApiResponse({ status: 200, description: 'Student found by ID' })
+  @ApiResponse({ status: 404, description: 'Student not found' })
   findOne(@Param('id') id: string) {
     return this.studentsService.findOne(+id);
   }
 
+  // PATCH /students/:id
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a student by ID' })
+  @ApiResponse({ status: 200, description: 'Student successfully updated' })
+  @ApiResponse({ status: 404, description: 'Student not found' })
   update(@Param('id') id: string, @Body() updateStudentDto: UpdateStudentDto) {
     return this.studentsService.update(+id, updateStudentDto);
   }
 
+  // DELETE /students/:id
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a student by ID' })
+  @ApiResponse({ status: 200, description: 'Student successfully deleted' })
+  @ApiResponse({ status: 404, description: 'Student not found' })
   remove(@Param('id') id: string) {
     return this.studentsService.remove(+id);
   }
