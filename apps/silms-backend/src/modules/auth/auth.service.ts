6f50d6cf9bb3740cd6c68c8b7ec9336a2cd1e50d
georgeuwagbale <uwagbalegeorge@gmail.com>
2023-12-23 23:05:27 +0100

    Added Jwt-auth guard Added Local-auth guard Added Passport Local Strategy Added Passport Jwt Strategy

--- apps/silms-backend/src/modules/auth/auth.service.ts
+++ apps/silms-backend/src/modules/auth/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
 import { UsersService } from '@modules/users/users.service';
 import { JwtService } from '@nestjs/jwt';
 
