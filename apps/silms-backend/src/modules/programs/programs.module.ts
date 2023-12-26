2c230fa8160668078b5a8cbf4e7708d0a071dade
OGB <chidubem.info@gmail.com>
2023-12-26 15:25:58 +0100

    create school endpoint

--- apps/silms-backend/src/modules/programs/programs.module.ts
+++ apps/silms-backend/src/modules/programs/programs.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProgramsService } from './programs.service';
 import { ProgramsController } from './programs.controller';
-
+import {  Program } from '@modules/programs/entities/program.entity'; 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Program]),
+  ],
   controllers: [ProgramsController],
   providers: [ProgramsService],
 })
--- apps/silms-backend/src/modules/schools/schools.service.ts
+++ apps/silms-backend/src/modules/schools/schools.service.ts
@@ -1,12 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSchoolDto } from './dto/create-school.dto';
 import { UpdateSchoolDto } from './dto/update-school.dto';
-
+import { Repository } from 'typeorm';
+import { School } from '@modules/schools/entities/school.entity'; 
+import {  Program } from '@modules/programs/entities/program.entity'; 
+import { InjectRepository } from '@nestjs/typeorm';
 @Injectable()
 export class SchoolsService {
-  create(createSchoolDto: CreateSchoolDto) {
-    return 'This action adds a new school';
+  constructor (@InjectRepository(School) private readonly schoolRepository: Repository<School>){}
+  create(createSchoolDto: CreateSchoolDto){
+  try {
+    const school = new School();
+    school.name = createSchoolDto.name;
+    school.abbreviation = createSchoolDto.abbreviation;
+
+    const programs = createSchoolDto.programs.map((programName: string) => {
+      const program = new Program();
+      program.programName = programName;
+      return program;
+    });
+
+    school.programs = programs;
+    const savedSchool = this.schoolRepository.save(school);
+    return savedSchool;
+  } catch (error) {
+    return { message: "Failed to create school", error: error.message || error };
   }
+}
+
 
   findAll() {
     return `This action returns all schools`;
