5f8fc9ffddedb23fa13227e1d836b61f2a5e6312
francisojeah <ojeahfrancis@gmail.com>
2023-12-31 23:30:40 +0100

    implemented create coursemodule dto

--- apps/silms-backend/src/modules/course-modules/dto/create-course-module.dto.ts
+++ apps/silms-backend/src/modules/course-modules/dto/create-course-module.dto.ts
@@ -1,1 +1,14 @@
-export class CreateCourseModuleDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateCourseModuleDto {
+  @ApiProperty()
+  @IsNotEmpty({ message: 'Module number is required' })
+  @IsNumber({}, { message: 'Module number should be a number' })
+  readonly moduleNumber: number;
+
+  @ApiProperty()
+  @IsNotEmpty({ message: 'Module title is required' })
+  @IsString()
+  readonly moduleTitle: string;
+}
