9634674a77be1519ad8b2292734b37fc09728cd1
francisojeah <ojeahfrancis@gmail.com>
2023-12-31 04:50:04 +0100

    implemented create student course dto

--- apps/silms-backend/src/modules/student-courses/dto/create-student-course.dto.ts
+++ apps/silms-backend/src/modules/student-courses/dto/create-student-course.dto.ts
@@ -1,1 +1,11 @@
-export class CreateStudentCourseDto {}
+export class CreateStudentCourseDto {
+    readonly studentId: string;
+    readonly courseId: string;
+    readonly enrollmentDate?: Date;
+    readonly completionDate?: Date;
+    readonly grade?: string;
+    readonly status?: string;
+    readonly progress?: number;
+    readonly attendance?: boolean;
+  }
+  
