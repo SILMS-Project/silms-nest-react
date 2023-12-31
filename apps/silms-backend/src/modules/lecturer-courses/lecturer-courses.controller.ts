203f482d0e428ec12e4f48abd7f62394e844ba4e
georgeuwagbale <uwagbalegeorge@gmail.com>
2023-12-31 19:22:48 +0100

    added the functionality for assgining courses to lecturers

--- apps/silms-backend/src/modules/lecturer-courses/lecturer-courses.controller.ts
+++ apps/silms-backend/src/modules/lecturer-courses/lecturer-courses.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, Version } from '@nes
 import { LecturerCoursesService } from './lecturer-courses.service';
 import { CreateLecturerCourseDto } from './dto/create-lecturer-course.dto';
 import { UpdateLecturerCourseDto } from './dto/update-lecturer-course.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('lecturer-course')
 @Controller('lecturer-courses')
@@ -10,6 +10,8 @@ export class LecturerCoursesController {
   constructor(private readonly lecturerCoursesService: LecturerCoursesService) {}
 
   @Version('1')
+  @ApiOperation({ summary: 'Assign a course to a lecturer' })
+  @ApiResponse({ status: 201, description: 'Course assigned to lectuere successfully' })
   @Post('create')
   create(@Body() createLecturerCourseDto: CreateLecturerCourseDto) {
     return this.lecturerCoursesService.create(createLecturerCourseDto);
@@ -28,17 +30,20 @@ export class LecturerCoursesController {
   }
 
   @Version('1')
+  @ApiOperation({ summary: 'Get course(s) taken by a lecturer' })
   @Get('course/:lecturerId')
   async findCoursesByLecturer(@Param('lecturerId') lecturerId: string) {
     return await this.lecturerCoursesService.findCoursesByLecturer(lecturerId);
   }
 
   @Version('1')
+  @ApiOperation({ summary: 'Get lecturer(s) taking a course' })
   @Get('lecturer/:courseId')
   async findLecturersByCourse(@Param('courseId') courseId: string) {
     return this.lecturerCoursesService.findLecturersByCourse(courseId);
   }
 
+  @Version('1')
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateLecturerCourseDto: UpdateLecturerCourseDto) {
     return this.lecturerCoursesService.update(id, updateLecturerCourseDto);
