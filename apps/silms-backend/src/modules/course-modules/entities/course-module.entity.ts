22651e414747e13f580a4de21070f4bb739e7f40
francisojeah <ojeahfrancis@gmail.com>
2023-12-31 14:21:47 +0100

    implemented course module entity

--- apps/silms-backend/src/modules/course-modules/entities/course-module.entity.ts
+++ apps/silms-backend/src/modules/course-modules/entities/course-module.entity.ts
@@ -1,1 +1,31 @@
-export class CourseModule {}
+import { Assessment } from '@/modules/assessments/entities/assessment.entity';
+import { CourseContent } from '@/modules/course-contents/entities/course-content.entity';
+import { Course } from '@/modules/courses/entities/course.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  OneToMany,
+} from 'typeorm';
+
+@Entity()
+export class CoruseModule {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  moduleNumber: number;
+
+  @Column()
+  moduleTitle: string;
+
+  @ManyToOne(() => Course, (course) => course.courseModules)
+  course: Course;
+
+  @OneToMany(() => CourseContent, (courseContent) => courseContent.courseModule)
+  courseContents: CourseContent[];
+
+  @OneToMany(() => Assessment, (assessment) => assessment.courseModule)
+  assessments: Assessment[];
+}
