a3cd63d6d780aa21635bcdd91d25df98af06864e
francisojeah <ojeahfrancis@gmail.com>
2023-12-31 06:52:51 +0100

    updated course module to accomodate different course type

--- apps/silms-backend/src/modules/courses/courses.service.ts
+++ apps/silms-backend/src/modules/courses/courses.service.ts
@@ -8,18 +8,25 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class CoursesService {
-  constructor(@InjectRepository(Course) private readonly courseRepository: Repository<Course>,
-  @InjectRepository(Program) private readonly programRepository: Repository<Program>,){}
+  constructor(
+    @InjectRepository(Course)
+    private readonly courseRepository: Repository<Course>,
+    @InjectRepository(Program)
+    private readonly programRepository: Repository<Program>,
+  ) {}
 
   async create(createCourseDto: CreateCourseDto): Promise<Course> {
     const course = new Course();
     course.courseCode = createCourseDto.courseCode;
     course.courseTitle = createCourseDto.courseTitle;
+    course.courseType = createCourseDto.courseType;
     course.unit = createCourseDto.unit;
     course.semester = createCourseDto.semester;
     course.description = createCourseDto.description || null;
     course.image = createCourseDto.image || null;
-    const program = await this.programRepository.findOne({where:{id:createCourseDto.programId},});
+    const program = await this.programRepository.findOne({
+      where: { id: createCourseDto.programId },
+    });
     if (!program) {
       throw new Error(`Program with id ${createCourseDto.programId} not found`);
     }
@@ -28,7 +35,6 @@ export class CoursesService {
 
     return savedCourse;
   }
-
 
   findAll() {
     return `This action returns all courses`;
