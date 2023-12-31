c6a8bf6ba310dda17b27c87216b84dc7edeca34e
francisojeah <ojeahfrancis@gmail.com>
2023-12-31 23:31:48 +0100

    implemented create grade dto

--- apps/silms-backend/src/modules/grades/dto/create-grade.dto.ts
+++ apps/silms-backend/src/modules/grades/dto/create-grade.dto.ts
@@ -1,1 +1,9 @@
-export class CreateGradeDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber } from 'class-validator';
+
+export class CreateGradeDto {
+  @ApiProperty({ required: false })
+  @IsNotEmpty({ message: 'Grade is required' })
+  @IsNumber({}, { message: 'Grade should be a number' })
+  readonly grade?: number;
+}
