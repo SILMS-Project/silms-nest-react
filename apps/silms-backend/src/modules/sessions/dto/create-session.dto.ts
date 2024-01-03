550b8bf4a2245393f5dd38c4afa844e9647810b0
francisojeah <ojeahfrancis@gmail.com>
2024-01-03 13:45:48 +0100

    implemented dto for session and schedules modules

--- apps/silms-backend/src/modules/sessions/dto/create-session.dto.ts
+++ apps/silms-backend/src/modules/sessions/dto/create-session.dto.ts
@@ -1,1 +1,18 @@
-export class CreateSessionDto {}
+import { IsNotEmpty, IsBoolean, IsNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateSessionDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  sessionName: string;
+
+  @ApiProperty()
+  @IsBoolean()
+  isCurrentSession: boolean;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  semester: number;
+}
