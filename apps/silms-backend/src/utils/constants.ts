79a82783d52fa9398d67b6dbd02130f43a61b5dd
francisojeah <ojeahfrancis@gmail.com>
2024-01-03 09:55:15 +0100

    implemened the application dto and entity

--- apps/silms-backend/src/utils/constants.ts
+++ apps/silms-backend/src/utils/constants.ts
@@ -14,3 +14,10 @@ export enum CourseType {
   COMPULSORY = 'compulsory',
   ELECTIVE = 'elective',
 }
+
+export enum ApplicationStatus {
+  PENDING = 'Pending',
+  ACCEPTED = 'Accepted',
+  REJECTED = 'Rejected',
+  UNDER_REVIEW = 'Under Review',
+}
