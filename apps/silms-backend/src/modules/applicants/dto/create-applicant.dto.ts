6ffdb2c6aec333d6c79300b7f257efaff48162a1
francisojeah <ojeahfrancis@gmail.com>
2024-01-03 10:02:56 +0100

    implemented applicant dto and entity

--- apps/silms-backend/src/modules/applicants/dto/create-applicant.dto.ts
+++ apps/silms-backend/src/modules/applicants/dto/create-applicant.dto.ts
@@ -1,1 +1,14 @@
-export class CreateApplicantDto {}
+import { IsNotEmpty, IsString, IsDate } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateApplicantDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  nationalIdentification: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsDate()
+  dateOfBirth: Date;
+}
