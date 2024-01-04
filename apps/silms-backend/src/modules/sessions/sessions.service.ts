1802d231d30b08c06d4707df400611f78bf839d9
Lotanna Okeke <lotannaokeke@Lotannas-MacBook-Pro.local>
2024-01-04 19:53:43 +0100

    get session by id function added to session services

--- apps/silms-backend/src/modules/sessions/sessions.service.ts
+++ apps/silms-backend/src/modules/sessions/sessions.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateSessionDto } from './dto/create-session.dto';
 import { UpdateSessionDto } from './dto/update-session.dto';
+import { Session } from './entities/session.entity';
 
 @Injectable()
 export class SessionsService {
+  sessionRepository: any;
   create(createSessionDto: CreateSessionDto) {
     return 'This action adds a new session';
   }
@@ -14,6 +16,16 @@ export class SessionsService {
 
   findOne(id: number) {
     return `This action returns a #${id} session`;
+  }
+
+  async findById(id: string): Promise<Session> {
+    const session = await this.sessionRepository.findOne({ where: { id } });
+
+    if (!session) {
+      throw new NotFoundException(`Session with ID ${id} not found`);
+    }
+
+    return session;
   }
 
   update(id: number, updateSessionDto: UpdateSessionDto) {
