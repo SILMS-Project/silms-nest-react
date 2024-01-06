49085b9762aa97cb78641a22dc580fa5d3de31fa
Lotanna Okeke <lotannaokeke@Lotannas-MacBook-Pro.local>
2024-01-06 12:34:48 +0100

    get assessment submission method added to assessments services

--- apps/silms-backend/src/modules/assessments/assessments.service.ts
+++ apps/silms-backend/src/modules/assessments/assessments.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateAssessmentDto } from './dto/create-assessment.dto';
 import { UpdateAssessmentDto } from './dto/update-assessment.dto';
+import { Submission } from '../submissions/entities/submission.entity';
 
 @Injectable()
 export class AssessmentsService {
+  assessmentRepository: any;
   create(createAssessmentDto: CreateAssessmentDto) {
     return 'This action adds a new assessment';
   }
@@ -14,6 +16,16 @@ export class AssessmentsService {
 
   findOne(id: number) {
     return `This action returns a #${id} assessment`;
+  }
+
+  async getAssessmentSubmissions(assessmentId: string): Promise<Submission[]> {
+    const assessment = await this.assessmentRepository.findOne(assessmentId, { relations: ['submissions'] });
+
+    if (!assessment) {
+      throw new NotFoundException(`Assessment with ID ${assessmentId} not found`);
+    }
+
+    return assessment.submissions;
   }
 
   update(id: number, updateAssessmentDto: UpdateAssessmentDto) {
