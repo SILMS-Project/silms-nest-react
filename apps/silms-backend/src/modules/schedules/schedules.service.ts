524acfc103dfe589ffd66554375e674f772548d5
Chinwendu Nweje <chinwendu.nweje@pau.edu.ng>
2024-01-08 21:40:43 +0100

    implemented create schedules

--- apps/silms-backend/src/modules/schedules/schedules.service.ts
+++ apps/silms-backend/src/modules/schedules/schedules.service.ts
@@ -1,12 +1,43 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Schedule } from './entities/schedule.entity';
 import { CreateScheduleDto } from './dto/create-schedule.dto';
 import { UpdateScheduleDto } from './dto/update-schedule.dto';
+import { validate } from 'class-validator';
+
 
 @Injectable()
 export class SchedulesService {
-  create(createScheduleDto: CreateScheduleDto) {
-    return 'This action adds a new schedule';
+  constructor(
+    @InjectRepository(Schedule)
+    private readonly scheduleRepository: Repository<Schedule>,
+
+  ) {}
+  
+
+  async create(createScheduleDto: CreateScheduleDto): Promise<Schedule> {
+    const schedule = new Schedule();
+    // Map DTO properties to entity properties
+    
+    schedule.dayOfWeek = createScheduleDto.dayOfWeek;
+    schedule.startTime = createScheduleDto.startTime;
+    schedule.endTime = createScheduleDto.endTime;
+    schedule.room = createScheduleDto.room;
+
+    
+
+  // Validate the DTO
+  await validate(createScheduleDto);
+
+  const savedSchedule = await this.scheduleRepository.save(schedule);
+  return savedSchedule;
   }
+  
+  // create(createScheduleDto: CreateScheduleDto) {
+  //   return 'This action adds a new schedule';
+  // }
 
   findAll() {
     return `This action returns all schedules`;
