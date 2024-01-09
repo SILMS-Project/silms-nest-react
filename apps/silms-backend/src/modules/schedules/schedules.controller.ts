4580fd73332ca2301a3e24ca5eae23fdc4a14459
Chinwendu Nweje <chinwendu.nweje@pau.edu.ng>
2024-01-09 14:02:52 +0100

    added versioning for schedule controller

--- apps/silms-backend/src/modules/schedules/schedules.controller.ts
+++ apps/silms-backend/src/modules/schedules/schedules.controller.ts
@@ -6,38 +6,46 @@ import {
   Patch,
   Param,
   Delete,
+  Version,
 } from '@nestjs/common';
 import { SchedulesService } from './schedules.service';
 import { CreateScheduleDto } from './dto/create-schedule.dto';
 import { UpdateScheduleDto } from './dto/update-schedule.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('schedules')
 @Controller('schedules')
 export class SchedulesController {
   constructor(private readonly schedulesService: SchedulesService) {}
 
-  @Post()
+  @Version('1')
+  @Post('create')
   create(@Body() createScheduleDto: CreateScheduleDto) {
     return this.schedulesService.create(createScheduleDto);
   }
 
+  @Version('1')
   @Get()
   findAll() {
     return this.schedulesService.findAll();
   }
 
+  @Version('1')
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.schedulesService.findOne(+id);
   }
 
-  @Patch(':id')
+  @Version('1')
+  @Patch('update/:id')
   update(
     @Param('id') id: string,
     @Body() updateScheduleDto: UpdateScheduleDto,
   ) {
-    return this.schedulesService.update(+id, updateScheduleDto);
+    return this.schedulesService.update(id, updateScheduleDto);
   }
 
+  @Version('1')
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.schedulesService.remove(+id);
