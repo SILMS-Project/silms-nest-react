ca654494f67a3fb416ae770a50c9b65d3d3866e9
Chinwendu Nweje <chinwendu.nweje@pau.edu.ng>
2024-01-09 16:24:46 +0100

    added api operation and api response

--- apps/silms-backend/src/modules/schedules/schedules.controller.ts
+++ apps/silms-backend/src/modules/schedules/schedules.controller.ts
@@ -10,9 +10,10 @@ import {
   Version,
 } from '@nestjs/common';
 import { SchedulesService } from './schedules.service';
+import { Schedule } from './entities/schedule.entity';
 import { CreateScheduleDto } from './dto/create-schedule.dto';
 import { UpdateScheduleDto } from './dto/update-schedule.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiResponse  } from '@nestjs/swagger';
 
 @ApiTags('schedules')
 @Controller('schedules')
@@ -21,24 +22,39 @@ export class SchedulesController {
 
   @Version('1')
   @Post('create')
+  @ApiOperation({ summary: 'Create a new schedule' })
+  @ApiResponse({ status: 201, description: 'Schedule successfully created' })
   create(@Body() createScheduleDto: CreateScheduleDto) {
     return this.schedulesService.create(createScheduleDto);
   }
 
   @Version('1')
   @Get()
+  @ApiOperation({ summary: 'Get all schedules' })
+  @ApiResponse({
+    status: 200,
+    description: 'List of all schedules',
+    type: Schedule,
+    isArray: true,
+  })
   findAll() {
     return this.schedulesService.findAll();
   }
 
   @Version('1')
   @Get(':id')
+  @ApiOperation({ summary: 'Get a schedule by ID' })
+  @ApiResponse({ status: 200, description: 'Schedule found by ID' })
+  @ApiResponse({ status: 404, description: 'Schedule not found' })
   findOne(@Param('id') id: string) {
     return this.schedulesService.findOne(id);
   }
 
   @Version('1')
   @Patch('update/:id')
+  @ApiOperation({ summary: 'Update a schedule by ID' })
+  @ApiResponse({ status: 200, description: 'Schedule successfully updated' })
+  @ApiResponse({ status: 404, description: 'Schedule not found' })
   update(
     @Param('id') id: string,
     @Body() updateScheduleDto: UpdateScheduleDto,
@@ -48,6 +64,9 @@ export class SchedulesController {
 
   @Version('1')
   @Delete('delete/:id')
+  @ApiOperation({ summary: 'Delete a schedule by ID' })
+  @ApiResponse({ status: 200, description: 'Schedule successfully deleted' })
+  @ApiResponse({ status: 404, description: 'Schedule not found' })
   remove(@Param('id') id: string) {
     return this.schedulesService.remove(id);
   }
