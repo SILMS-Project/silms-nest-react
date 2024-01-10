09324d4781984f51e3deb60d2d9efe32644f40a7
francisojeah <ojeahfrancis@gmail.com>
2024-01-10 20:11:25 +0100

    implemented error 404 page

--- apps/silms-frontend/src/pages/Errors/NotFound.tsx
+++ apps/silms-frontend/src/pages/Errors/NotFound.tsx
@@ -15,8 +15,8 @@ const NotFound = () => {
 
   return (
     <div className="flex h-screen p-4 justify-center items-center">
-      <div className="w-full md:w-1/2 mx-auto p-16 md:p-64 mt-10 flex flex-col gap-8 justify-center items-center">
-        <p className="mt-8 text-4xl text-secondary font-bold text-center lg:text-6xl">
+      <div className="w-full md:w-1/2 mx-auto lg:p-16 lg:mt-10 flex flex-col gap-8 justify-center items-center">
+        <p className="lg:mt-8 lg:text-2xl text-secondary font-bold text-center">
           This page does not exist
         </p>
 
@@ -28,13 +28,13 @@ const NotFound = () => {
 
         <div className="w-full flex justify-between gap-5 mt-4">
           <button
-            className="w-1/2 px-4 py-2 rounded-lg bg-brand-primary text-white text-base font-medium hover:bg-transparent hover:text-brand-primary border-2 border-brand-primary focus:outline-none"
+            className="w-1/2 rounded-xl bg-custom-accent-1 text-custom-primary-1 text-base font-medium hover:bg-transparent hover:text-custom-accent-1 border-2 border-custom-accent-1 focus:outline-none"
             onClick={handleGoBack}
           >
             Go back
           </button>
           <button
-            className="w-1/2 px-4 py-2 rounded-lg bg-white text-brand-primary text-base font-medium hover:bg-brand-primary hover:text-white border-2 border-brand-primary focus:outline-none"
+            className="w-1/2 rounded-xl bg-white text-custom-accent-1 text-base font-medium hover:bg-custom-accent-1 hover:text-custom-primary-1  border-2 border-custom-accent-1 focus:outline-none"
             onClick={onClickGoHome}
           >
             Go home
--- apps/silms-frontend/tailwind.config.js
+++ apps/silms-frontend/tailwind.config.js
@@ -3,17 +3,39 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
   content: [
-  "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",
-  "../../node_modules/flowbite-react/**/*.{js,ts,jsx,tsx}"
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "../../node_modules/flowbite-react/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+
+      colors: {
+        custom: {
+          primary: {
+            1: "#063760",
+            2: "#597995",
+            3: "#ACBCCA",
+          },
+          secondary: {
+            1: "#B5D2CB",
+            2: "#CDE1DC",
+            3: "#E6F0ED",
+          },
+          accent: {
+            1: "#40E0BA",
+            2: "#7FEAD1",
+            3: "#BFF4E8",
+          },
+        },
+      },
+    },
     screens: {
       ...defaultTheme.screens,
     },
   },
-  plugins: [
-    require('flowbite/plugin')
-    ]
- }
+  plugins: [require("flowbite/plugin")],
+};
