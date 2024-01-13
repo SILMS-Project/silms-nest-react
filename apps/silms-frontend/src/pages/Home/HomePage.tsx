4f01324be69727d1b2c043a3db08223ae96d5df8
francisojeah <ojeahfrancis@gmail.com>
2024-01-13 12:27:30 +0100

    implemented redirect to dashboard from homepage

--- apps/silms-frontend/src/pages/Home/HomePage.tsx
+++ apps/silms-frontend/src/pages/Home/HomePage.tsx
@@ -1,6 +1,13 @@
 import Demo from "@/components/Demo";
+import { useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+
 
 const HomePage = () => {
+  const navigate = useNavigate();
+  useEffect(()=> {
+    navigate("/student-dashboard", { replace: true });
+  }, [])
   return (
     <>
       <Demo />
