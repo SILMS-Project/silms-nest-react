6e939053134a34823d75c6c4314559086cbff8d8
georgeuwagbale <uwagbalegeorge@gmail.com>
2024-01-17 21:16:27 +0100

    Add flowbite-datepicker package and update ScheduleUnit component

--- apps/silms-frontend/src/components/ScheduleUnit.tsx
+++ apps/silms-frontend/src/components/ScheduleUnit.tsx
@@ -13,32 +13,32 @@ interface LayoutProps {
 
 const ScheduleUnit = (props: LayoutProps) => {
   return (
-    <>
+    <div className="">
       <div className="flex flex-row space-x-4">
         <div>
           <p className="font-bold text-[0.7rem]">{props.startTime}</p>
         </div>
         <div
-          className={`rounded-md flex flex-row ${props.textColor} items-center ${props.bg}  h-[68px] px-3`}
+          className={`rounded-md flex flex-row ${props.textColor} items-center ${props.bg} w-[229px] h-[68px] px-3`}
         >
           {/* {props.icon} */}
           <ScheduleIcons icon={props.icon} />
-          <div className="flex flex-col ml-2 gap-1">
-            <p className="font-bold leading-none text-[0.9rem]">
+          <div className="flex flex-col ml-2 gap-0">
+            <p className="font-bold leading-none text-[0.9rem] w-[63px] h-[19px]">
               {props.courseCode}
             </p>
-            <p className="leading-none text-[0.5rem] font-bold">
+            <p className="leading-none text-[0.5rem] font-bold truncate w-[70px]">
               {props.lecturerName}
             </p>
           </div>
           <div className="flex flex-col items-start ml-5 mb-5">
-            <p className="leading-none text-[0.49rem] font-bold">
+            <p className="leading-none text-[0.49rem] font-bold w-[63px]">
               {props.startTime} - {props.endTime}
             </p>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
